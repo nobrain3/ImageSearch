d2e8e13494e112f83c3d98b8dfa50e9d2abcc534
nobrain3 <todyl@naver.com>
2019-03-21 00:42:50 +0900

    okhttp client 추가.

--- app/src/main/java/kr/co/kjworld/imagesearch/model/network/RetrofitInstance.java
+++ app/src/main/java/kr/co/kjworld/imagesearch/model/network/RetrofitInstance.java
@@ -1,22 +1,34 @@
 package kr.co.kjworld.imagesearch.model.network;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
-    private static Retrofit retrofit;
     private static final String BASE_URL = "https://dapi.kakao.com";
+
+    private static Retrofit retrofit;
     private static String searchString;
+    private static OkHttpClient client;
+
 
     /**
      * Create an instance of Retrofit object
      * */
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
+
         }
         return retrofit;
     }
